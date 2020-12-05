6b31639e33fec4247923c9fb15b5b3fe1340d157
Houyuan Sha <petersha749@outlook.com>
2020-12-05 15:36:34 -0600

    update name and email identification, update data

--- setup.py
+++ setup.py
@@ -1,9 +1,32 @@
 from elasticsearch import Elasticsearch
 from datetime import datetime
 import re
+import spacy
+
+def extract_name(input_string):
+    nlp = spacy.load('en')
+    doc = nlp(input_string)
+    for sentence in doc.ents:
+        # print(sentence, sentence.label_)
+        if sentence.label_ == "PERSON":
+            return str(sentence)
+    return "Not Found"
+
+def extract_email(input_string):
+    nlp = spacy.load('en')
+    doc = nlp(input_string)
+    for sentence in doc:
+        if (sentence.like_email):
+            return str(sentence)
+
+    match = re.search(r'[\w\.-]+@[\w\.-]+\.\w+', input_string)
+    if match:
+        return match.group()
+
+    return "Not Found"
 
 bios = []
-with open("origin_data/compiled_dataset/bios.txt", "r") as f:
+with open("origin_data/bios6525.txt", "r") as f:
     lines = f.readlines()
     for line in lines:
         line = line.strip()
@@ -11,7 +34,7 @@ with open("origin_data/compiled_dataset/bios.txt", "r") as f:
         bios.append(line)
 
 urls = []
-with open("origin_data/compiled_dataset/urls.txt", "r") as f:
+with open("origin_data/urls6525.txt", "r") as f:
     lines = f.readlines()
     for line in lines:
         line = line.strip()
@@ -24,15 +47,14 @@ for i in range(len(bios)):
     url = urls[i]
     bio = bios[i]
 
-    email = "None"
-    match = re.search(r'[\w\.-]+@[\w\.-]+\.\w+', bio)
-    if match:
-        email = match.group()
+    # email = "None"
+    # match = re.search(r'[\w\.-]+@[\w\.-]+\.\w+', bio)
+    # if match:
+    #     email = match.group()
 
     doc = {
-        'author': 'Houyuan Sha',
-        'faculty_name': "Dr. Ruby Wang (place_holder)",
-        'faculty_email': email,
+        'faculty_name': extract_name(bio),
+        'faculty_email': extract_email(bio),
         'faculty_url': url,
         'text': bio,
         'timestamp': datetime.now(),
